0ddb0e2c134e000b83086136b0f4e8662eeb2802
Rahmattullah <rfkrhmttllh@gmail.com>
2023-12-20 09:54:09 +0700

    pass 2nd stage

--- app/main.go
+++ app/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 )
 
 func createDNSHeader() []byte {
+	// Header fields
 	packetID := uint16(1234)
-	qr := byte(1) << 1
+	qr := byte(1) << 7 // 1 for response packet
 	opcode := byte(0)
 	aa := byte(0)
 	tc := byte(0)
@@ -17,10 +17,11 @@ func createDNSHeader() []byte {
 	z := byte(0)
 	rcode := byte(0)
 	qdcount := uint16(0)
-	ancount := uint16(0)
+	ancount := uint16(1) // Assuming one answer record
 	nscount := uint16(0)
 	arcount := uint16(0)
 
+	// Pack the header fields into a byte slice
 	header := []byte{
 		byte(packetID >> 8), byte(packetID),
 		qr | (opcode << 3) | (aa << 2) | (tc << 1) | rd,
@@ -34,11 +35,29 @@ func createDNSHeader() []byte {
 	return header
 }
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+func createAnswerRecord() []byte {
+	// Sample answer record
+	name := []byte{0xc0, 0x0c}            // Compressed name pointer to the domain name in the question
+	qtype := []byte{0x00, 0x01}           // A record type
+	qclass := []byte{0x00, 0x01}          // IN class
+	ttl := []byte{0x00, 0x00, 0x00, 0x0a} // 10 seconds
+	rdlength := []byte{0x00, 0x04}        // Length of the RData field (IPv4 address)
 
-	// Uncomment this block to pass the first stage
+	// IPv4 address (replace with your actual IP address)
+	rdata := net.ParseIP("127.0.0.1").To4()
+
+	// Combine all components into the answer record
+	answerRecord := append(name, qtype...)
+	answerRecord = append(answerRecord, qclass...)
+	answerRecord = append(answerRecord, ttl...)
+	answerRecord = append(answerRecord, rdlength...)
+	answerRecord = append(answerRecord, rdata...)
+
+	return answerRecord
+}
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
 
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
@@ -65,14 +84,10 @@ func main() {
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
-		// create a dns response with the specified header
+		// Create a DNS response with the specified header and answer record
 		responseHeader := createDNSHeader()
-
-		// combine the header and any additional response data
-		response := append(responseHeader, []byte("Your additional response data here")...)
-
-		// Create an empty response
-		response = []byte{}
+		answerRecord := createAnswerRecord()
+		response := append(responseHeader, answerRecord...)
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
